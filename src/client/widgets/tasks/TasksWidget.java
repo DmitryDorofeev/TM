cbc15021c9508de967ee32d4141a8412a43781a0
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2015-04-28 14:33:43 +0300

    fix styles, cell view

--- src/client/widgets/tasks/TasksWidget.java
+++ src/client/widgets/tasks/TasksWidget.java
@@ -1,12 +1,15 @@
 package client.widgets.tasks;
 
 import client.TasksService;
+import client.cells.tasks.TasksCell;
+import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -16,6 +19,10 @@ import org.fusesource.restygwt.client.Method;
 import org.fusesource.restygwt.client.MethodCallback;
 import shared.Response;
 import shared.ResponseTasks;
+import shared.Task;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by dmitry on 27.04.15.
@@ -25,13 +32,14 @@ public class TasksWidget extends Composite {
     @UiField
     DivElement preloader;
     @UiField
-    TasksStyle style;
+    TasksStyle dynamic;
     @UiField
     FlowPanel content;
 
     interface TasksStyle extends CssResource {
         String hidden();
     }
+
     interface TasksUiBinder extends UiBinder<Widget, TasksWidget> {
     }
 
@@ -47,9 +55,10 @@ public class TasksWidget extends Composite {
 
             public void onSuccess(Method method, ResponseTasks response) {
                 content.clear();
-                for (int i = 0; i < response.data.length; ++i) {
-                    Window.alert(response.data[i].title);
-                }
+                CellList<Task> tasks = new CellList<Task>(new TasksCell());
+                List<Task> list = Arrays.asList(response.data);
+                tasks.setRowData(0, list);
+                content.add(tasks);
             }
         });
     }
